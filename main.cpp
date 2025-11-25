adaab7ded39f3e5691702b9b427dd5a1cbbcc3dd
pabloarshid <umarsa.birvancity@gmail.com>
2025-11-25 14:33:40 -0800

    test: Verify GPRMC parsing in driver

--- main.cpp
+++ main.cpp
@@ -14,7 +14,8 @@ int main() {
     std::vector<std::string> gpsStream = {
         "$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47", // Valid Fix
         "$GPGGA,123520,4807.040,N,01131.010,E,1,08,0.9,545.4,M,46.9,M,,*00", // Checksum Error
-        "$GPGGA,,,,,,0,,,,,,"                                              // No Satellite Lock
+        "$GPGGA,,,,,,0,,,,,,",                                              // No Satellite Lock
+        "$GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A"
     };
 
     // 3. The Main Loop
